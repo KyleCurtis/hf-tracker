fb860a0786678970296b3903f3d3ac6eae09beae
Kyle Curtis <30717363+KyleCurtis@users.noreply.github.com>
2023-03-02 12:15:54 -0500

    Fixed schema and API

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -1,49 +1,4 @@
 /*
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    return await addUser(req, res);
-  } else if (req.method === "GET") {
-    return await ReadUser(req, res);
-  } else {
-    return res
-      .status(405)
-      .json({ message: "Method not allowed", success: false });
-  }
-}
-
-async function readUser(req, res) {
-  const body = req.body;
-  try {
-    const users = await prisma.user.findMany();
-    return res.status(200).json(users, {success: true});
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({error: "Error reading from DB", success: false});
-  }
-}
-
-async function addUser(req, res) {
-  const body = req.body;
-  try {
-    const newEntry = await prisma.user.create({
-      data: {
-        email: body.email,
-        name: body.name,
-        password: body.password,
-      },
-    });
-    return res.status(200).json(newEntry, { success: true });
-  } catch (error) {
-    console.error("Request error", error);
-    res.status(500).json({ error: "Error creating user", success: false });
-  }
-} */
-
-/*
 this code defines a HTTP request handler function that interacts with a Prisma 
 database client to retrieve or add user records to the database, depending on the 
 HTTP method of the incoming request. The getUser function retrieves all user records 
@@ -56,7 +11,7 @@ error message in JSON format.
 */
 
 // Import the Prisma database client from the "/config/db" module
-import {prisma} from "/config/db"
+import { prisma } from "/config/db";
 
 // Define the main HTTP request handler function that handles GET and POST requests
 export default async function handler(req, res) {
@@ -87,25 +42,21 @@ const getUser = async (req, res) => {
 
 // Define a sub-function that creates a new user record in the database
 const addUser = async (req, res) => {
+  const body = req.body;
   try {
     // Extract the email, firstName, lastName, username, and password properties from the request body
-    const {email, firstName, lastName, username, password} = req.body;
+    const { email, firstName, lastName, username, password } = req.body;
 
     // Define a data object with properties matching the database schema
-    const data = {
-      email: email,
-      firstName: firstName,
-      lastName: lastName,
-      username: username,
-      password: password,
-    };
-
     // Use the Prisma client to create a new user record in the database with the data object
     const result = await prisma.userRequired.create({
-      data: data,
-      select: {
-        id: true
-      }
+      data: {
+        email: body.email,
+        firstName: body.firstName,
+        lastName: body.lastName,
+        username: body.username,
+        password: body.password,
+      },
     });
 
     // Return a response with HTTP status code 200 and the newly created record's ID in JSON format
