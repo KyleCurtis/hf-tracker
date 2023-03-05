01872bf479c3fb777314798993dfa0645dcf62d0
Kyle Curtis <30717363+KyleCurtis@users.noreply.github.com>
2023-03-05 18:52:11 -0500

    Revert "Type fix"
    This reverts commit 7ef7819809d470ea19eb399070a4840fe9784674.

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -14,11 +14,11 @@ if (typeof window === "undefined") {
   if (process.env.NODE_ENV === "production") {
     prisma = new PrismaClient();
   } else {
-    if (!(global as any).prisma) {
-      (global as any).prisma = new PrismaClient();
+    if (!global.prisma) {
+      global.prisma = new PrismaClient();
     }
 
-    prisma = (global as any).prisma;
+    prisma = global.prisma;
   }
 }
 
