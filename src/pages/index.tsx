7588ed02839ea36fd87ccf88d99941e66e314ea3
steffensq <11350946+steffensq@users.noreply.github.com>
2023-03-08 16:18:28 -0500

    update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { getSession, signOut } from "next-auth/react";
-import UnlockedHome from "@/components/UnlockedHome";
-import LockedHome from "@/components/LockedHome";
-import Hero from "@/components/Hero";
-import Image from "next/image";
-
-
-export default function Home({ data }: any) {
-  if (data) {
-=======
 import { useSession } from "next-auth/react";
 import UnlockedHome from "@/components/UnlockedHome";
 import LockedHome from "@/components/LockedHome";
@@ -19,19 +8,8 @@ export default function Home() {
     return <h1>Loading Dashboard...</h1>;
   }
   if (session) {
->>>>>>> c585eaca95a86df794aa225c627fb86fd7ed0b9a
     return <UnlockedHome />;
   } else {
     return <LockedHome />;
   }
-}
-
-export async function getServerSideProps(context: any) {
-  const session = await getSession(context);
-    
-  return {
-    props: {
-      data: session,
-    },
-  };
 }
